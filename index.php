7af69507ba23afa58093875b14eb364208ec1ae1
sereneress <pinarb10@gmail.com>
2025-02-24 12:32:39 +0700

    mengupload fitur register

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-ini index login
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <meta http-equiv="refresh" content="1; url=inc/login.php">
+</body>
+</html>
